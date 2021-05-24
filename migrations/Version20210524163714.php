1bf41347a704d7adcfd4900c0f176ca21bcf712c
codbear <douglas.meunier@codbear.com>
2021-05-24 18:39:21 +0200

    fix(migrations): refactor migrations to create binding format table

--- migrations/Version20210524163714.php
+++ migrations/Version20210524163714.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210524161441 extends AbstractMigration
+final class Version20210524163714 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,6 +20,8 @@ final class Version20210524161441 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE binding_format (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE book ADD binding_format_id INT DEFAULT NULL');
         $this->addSql('ALTER TABLE book ADD CONSTRAINT FK_CBE5A331F9E53BFC FOREIGN KEY (binding_format_id) REFERENCES binding_format (id)');
         $this->addSql('CREATE INDEX IDX_CBE5A331F9E53BFC ON book (binding_format_id)');
     }
@@ -28,6 +30,8 @@ final class Version20210524161441 extends AbstractMigration
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE book DROP FOREIGN KEY FK_CBE5A331F9E53BFC');
+        $this->addSql('DROP TABLE binding_format');
         $this->addSql('DROP INDEX IDX_CBE5A331F9E53BFC ON book');
+        $this->addSql('ALTER TABLE book DROP binding_format_id');
     }
 }
